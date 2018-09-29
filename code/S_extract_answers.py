e5b1dcca713010be1b338662136174476c29b89e
Sarthika Dhawan <sarthika15170@iiitd.ac.in>
2018-09-29 23:12:10 +0530

    Update S_extract_answers.py

--- code/S_extract_answers.py
+++ code/S_extract_answers.py
@@ -3,6 +3,12 @@ import re
 from rake_nltk import Rake
 from gensim.models import KeyedVectors
 
+# inputg = '../data/glove/glove.6B.300d.txt' # file used to make word vectors; can use some other sentence sets
+input_wordvector_embeddings = 'wrd2vec' #wrd2vec is glove vectors of the words file
+# glove2word2vec(inputg,input_wordvector_embeddings)
+model = KeyedVectors.load_word2vec_format(input_wordvector_embeddings, binary=False)
+print "glove loading done"
+
 r = Rake() 
 
 def jaccard(a,b):
@@ -11,8 +17,6 @@ def jaccard(a,b):
     b1=set(b)
     return len(a1.intersection(b1))/(1.0*len((a1).union(b1)))
 
-def sen_sim(a,b):
-    return 0
 
 def cleantext(text):
   cleanr = re.compile('<.*?>')
@@ -26,7 +30,7 @@ def get_qna(query,querytags):
   with open('tagged_qna.json', 'r') as f:
       qadict = json.load(f)    
       for index in qadict:
-          ans=jaccard(qadict[index]['tag'],querytags)+sen_sim(qadict[index]['ans'],query)
+          ans=jaccard(qadict[index]['tag'],querytags)+sentence_similarilty_wmd(model,qadict[index]['ans'],query)
           if ans>maxx:
               maxx=ans
               maxx_qna=qadict[index]['ans']
@@ -44,7 +48,7 @@ def get_quote(query,querytags):
       for book in quotesdict:        
           qlist=quotesdict[book]
           for quote in qlist:
-              ans=jaccard(quote['tag'],querytags)+sen_sim(quote['quote'],query)
+              ans=jaccard(quote['tag'],querytags)+sentence_similarilty_wmd(model,quote['quote'],query)
               if ans>maxx:
                   maxx=ans
                   maxx_quote=quote['quote']
@@ -56,11 +60,6 @@ def sentence_similarilty_wmd(model,sentence1,sentence2): # lower score means mor
   sentence2 = sentence2.lower().split()
   return model.wmdistance(sentence1, sentence2)
 
-# inputg = '../data/glove/glove.6B.300d.txt' # file used to make word vectors; can use some other sentence sets
-input_wordvector_embeddings = 'wrd2vec' #wrd2vec is glove vectors of the words file
-# glove2word2vec(inputg,input_wordvector_embeddings)
-model = KeyedVectors.load_word2vec_format(input_wordvector_embeddings, binary=False)
-print "glove loading done"
 
 query=raw_input("Enter query\n")         
 tag=r.extract_keywords_from_text(query)
@@ -68,6 +67,5 @@ ranked_tags=r.get_ranked_phrases()
 print 'Quote='+str(get_quote(query,ranked_tags))
 print 'QNA='+str(get_qna(query,ranked_tags))
 
-print sentence_similarilty_wmd(model,"this is a test sentence","testing is important task")
     
 
